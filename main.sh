83729911bceb2e7ab0460b3acffc8c8885c9abbd
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:17:22 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,14 +2,7 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-#apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
-
 wget -nv https://github.com/hyprwm/Hyprland/releases/download/v0.26.0/source-v0.26.0.tar.gz
 tar -xf ./source-v0.26.0.tar.gz
 cp -rvf ./debian ./hyprland-source
@@ -19,8 +12,6 @@ cd ./hyprland-source
 sed -E -i -e 's/(soversion = 12)([^032]|$$)/soversion = 12032/g' subprojects/wlroots/meson.build
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
