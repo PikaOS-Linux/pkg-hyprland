7d9b5ae577099de4ef0a37983bc8c5bd20f0497f
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-01 19:11:11 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,11 +5,11 @@ DEBIAN_FRONTEND=noninteractive
 # Clone Upstream
 wget -nv https://github.com/hyprwm/Hyprland/releases/download/v.0/source-v.0.tar.gz
 tar -xf ./source-v.0.tar.gz
-cp -rvf ./debian ./hyprland-source
+cp -rvf ./debian ./hyprland-source/
 cd ./hyprland-source
 
 #sed -i 's/\/usr\/local/\/usr/g' config.mk
-sed -E -i -e 's/(soversion = 12)([^032]|$$)/soversion = 12032/g' subprojects/wlroots/meson.build
+#sed -E -i -e 's/(soversion = 12)([^032]|$$)/soversion = 12032/g' subprojects/wlroots/meson.build
 
 # Get build deps
 apt-get update
